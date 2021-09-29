315bfe6c769b7b961fc6165303eae161f861b2b2
Alastair Hinde <alastairhinde@Alastairs-MacBook-Pro.local>
2021-09-29 09:40:54 +0100

    added local map of values for storage space alarm to set based on instance typype. Added sns permission to trigger lambda function.

---
+++ locals.tf
@@ -0,0 +1,8 @@
+locals {
+  instance_types = {
+    "i3.large.elasticsearch" = "118750"
+    "i3.xlarge.elasticsearch" = "237500"
+    "i3.2xlarge.elasticsearch" = "475000"
+    "i3.4xlarge.elasticsearch" = "950000"
+  }
+}
