6411f0e2cb14fdb14c9c53d982775af1cb484fb7
Amir <unknown>
2021-12-21 15:33:29 +0000

    addad kinesis_get_records_success and kinesis_put_records_success

--- cloudwatch-alarm.tf
+++ cloudwatch-alarm.tf
@@ -1,3 +1,37 @@
+resource "aws_cloudwatch_metric_alarm" "kinesis_get_records_success" {
+  count                     = var.kinesis_alarms_required ? 1 : 0
+  alarm_name                = "${var.alert_level}-${var.kinesis_stream_name}-kinesis-get-record-success"
+  comparison_operator       = "LessThanOrEqualToThreshold"
+  metric_name               = "GetRecords.Success"
+  namespace                 = "AWS/Kinesis"
+  statistic                 = "Average"
+  evaluation_periods        = var.get_records_success_evaluation_periods
+  period                    = var.get_records_success_period
+  threshold                 = var.get_records_success_threshold
+  alarm_description         = "The ${var.alert_level} alarm to monitor the kinesis successful GetRecords operations for ${var.kinesis_stream_name}"
+  alarm_actions             = var.alarm_actions
+  dimensions = {
+    StreamName = var.kinesis_stream_name
+  }
+}
+
+resource "aws_cloudwatch_metric_alarm" "kinesis_put_records_success" {
+  count                     = var.kinesis_alarms_required ? 1 : 0
+  alarm_name                = "${var.alert_level}-${var.kinesis_stream_name}-kinesis-put-record-success"
+  comparison_operator       = "GreaterThanOrEqualToThreshold"
+  metric_name               = "PutRecords.Success"
+  namespace                 = "AWS/Kinesis"
+  statistic                 = "Average"
+  evaluation_periods        = var.put_records_success_evaluation_periods
+  period                    = var.put_records_success_period
+  threshold                 = var.put_records_success_threshold
+  alarm_description         = "The ${var.alert_level} alarm to monitor the kinesis successful PutRecords operations for ${var.kinesis_stream_name}"
+  alarm_actions             = var.alarm_actions
+  dimensions = {
+    StreamName = var.kinesis_stream_name
+  }
+}
+
 resource "aws_cloudwatch_metric_alarm" "kinesis_get_iterator_age" {
   count                     = var.kinesis_alarms_required ? 1 : 0
   alarm_name                = "${var.alert_level}-${var.kinesis_stream_name}-kinesis-iterator-age"
@@ -5,8 +39,8 @@ resource "aws_cloudwatch_metric_alarm" "kinesis_get_iterator_age" {
   evaluation_periods        = var.get_iterator_evaluation_periods
   metric_name               = "GetRecords.IteratorAgeMilliseconds"
   namespace                 = "AWS/Kinesis"
-  period                    = var.get_iterator_period 
-  statistic                 = "Average" 
+  period                    = var.get_iterator_period
+  statistic                 = "Average"
   threshold                 = var.get_iterator_threshold
   alarm_description         = "The ${var.alert_level} alarm to monitor the kinesis get iterator age metric for ${var.kinesis_stream_name}"
   alarm_actions             = var.alarm_actions
@@ -18,11 +52,11 @@ resource "aws_cloudwatch_metric_alarm" "kinesis_get_iterator_age" {
 resource "aws_cloudwatch_metric_alarm" "kinesis_read_throughput" {
   count                     = var.kinesis_alarms_required ? 1 : 0
   alarm_name                = "${var.alert_level}-${var.kinesis_stream_name}-kinesis-read-throughput"
-  comparison_operator       = "GreaterThanThreshold"
+  comparison_operator       = "GreaterThanOrEqualToThreshold"
   evaluation_periods        = var.read_throughput_evaluation_periods
   metric_name               = "ReadProvisionedThroughputExceeded"
   namespace                 = "AWS/Kinesis"
-  period                    = var.read_throughput_period 
+  period                    = var.read_throughput_period
   statistic                 = "Average"
   threshold                 = var.read_throughput_threshold
   alarm_description         = "The ${var.alert_level} alarm to monitor the kinesis get iterator age metric for ${var.kinesis_stream_name}"
@@ -35,12 +69,12 @@ resource "aws_cloudwatch_metric_alarm" "kinesis_read_throughput" {
 resource "aws_cloudwatch_metric_alarm" "kinesis_write_throughput" {
   count                     = var.kinesis_alarms_required ? 1 : 0
   alarm_name                = "${var.alert_level}-${var.kinesis_stream_name}-kinesis-write-throughput"
-  comparison_operator       = "GreaterThanThreshold"
+  comparison_operator       = "GreaterThanOrEqualToThreshold"
   evaluation_periods        = var.write_throughput_evaluation_periods
   metric_name               = "WriteProvisionedThroughputExceeded"
   namespace                 = "AWS/Kinesis"
-  period                    = var.write_throughput_period 
-  statistic                 = "Average" 
+  period                    = var.write_throughput_period
+  statistic                 = "Average"
   threshold                 = var.write_throughput_threshold
   alarm_description         = "The ${var.alert_level} alarm to monitor the kinesis get iterator age metric for ${var.kinesis_stream_name}"
   alarm_actions             = var.alarm_actions
@@ -56,7 +90,7 @@ resource "aws_cloudwatch_metric_alarm" "cluster_yellow" {
   evaluation_periods        = "1"
   metric_name               = "ClusterStatus.yellow"
   namespace                 = "AWS/ES"
-  period                    = "60" 
+  period                    = "60"
   statistic                 = "Maximum"
   threshold                 = "1"
   alarm_description         = "The ${var.alert_level} alarm to monitor the AWS Opensearch cluster status is yellow for ${var.opensearch_domain}"
