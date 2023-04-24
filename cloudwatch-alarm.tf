9db8382f19ae6f4e0e7ebf4ce120b1b949e41ee0
Iqbal Shaikh <iqbal.shaikh@digital.homeoffice.gov.uk>
2023-04-24 15:52:32 +0100

    ACPENG-564 - updating description

--- cloudwatch-alarm.tf
+++ cloudwatch-alarm.tf
@@ -164,7 +164,7 @@ resource "aws_cloudwatch_metric_alarm" "cpu_utilization_alert" {
   period              = var.cpu_utilization_period
   statistic           = "Maximum"
   threshold           = var.cpu_utilization_threshold
-  alarm_description   = "AWS Opensearch cluster cpu utilization is consistently above ${var.cpu_utilization_threshold}% for The ${var.environment} environment for ${var.opensearch_domain} domain. If consistently being triggered it could mean that cluster needs to be scaled up or have different nodes."
+  alarm_description   = "AWS Opensearch cluster cpu utilization is consistently above ${var.cpu_utilization_threshold}%"
   alarm_actions       = var.alarm_actions
   dimensions = {
     DomainName = var.opensearch_domain
